759120bfed76645a57cc300b0273f2871cb82e2c
Iris Tech <iristech@pop-os.localdomain>
2020-08-24 22:18:46 +0500

    Initial Commit for Image Search App

---
+++ precache-manifest.e3e89ce823da8a2f74210148b6ff6e06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cc8d561920d3d50a462d69f09a0e1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60c4e6ec94a00fbcd280",
+    "url": "/static/css/main.0e716cf7.chunk.css"
+  },
+  {
+    "revision": "cb90233ca8eeefe13ea6",
+    "url": "/static/js/2.ad9a538b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad9a538b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c4e6ec94a00fbcd280",
+    "url": "/static/js/main.9e84c875.chunk.js"
+  },
+  {
+    "revision": "98faa1f90ba787d3562e",
+    "url": "/static/js/runtime-main.203a6646.js"
+  }
+]);
